3f5106c4c93fccad524d75a8c65bc9e7ed92470a
Martin Heermance <mheermance@gmail.com>
2026-01-27 19:50:05 -0500

    Debugged functions: pbHigh, pbInput, pbLow, pbOutput, pbPause, pbToggle Writing pbCount, pbPulsout

--- Mench Reloaded/pbasic.asm
+++ Mench Reloaded/pbasic.asm
@@ -12,19 +12,32 @@ __pbasic_asm__ = 1
 .include "common.inc"
 .include "pbasic.inc"
 .include "via.inc"
-.include "w65c265Monitor.inc"
 
 ;
 ; Aliases
 ;
+CLK_FREQ = 3686400		; cycles per second
+ONE_MS = CLK_FREQ / 1000	; cycles per ms
+ONE_US = CLK_FREQ / 1000000	; cycles per us
+T2IF = $20			; Timer 2 interupt flag bit
 
 ;
 ; Macros
 ;
 
 ;
+; Static data
+;
+bitsetMask:	.word $0100, $0200, $0400, $0800, $1000, $2000, $4000, $8000
+		.word $0001, $0002, $0004, $0008, $0010, $0020, $0040, $0080
+
+;
 ; Functions
 ;
+
+; Debugged: pbHigh, pbInput, pbLow, pbOutput, pbPause, pbToggle
+; Todo: pbCount, pbFreqOut, pbPulsin, pbPulsout, pbRCTime
+
 
 ; pbCount - counts the number of cycles (0-1-0 or 1-0-1) on the specified pin
 ; during the duration time frame and return that number.
@@ -34,6 +47,47 @@ __pbasic_asm__ = 1
 ; Outputs:
 ;   A - the number of transitions.
 PUBLIC pbCount
+	LAST_VALUE = 0		; stack offsets for local variables.
+	PIN_MASK = 2
+	COUNT = 4
+	pea $0000		; initialize count stack local
+	txy			; save time parameter to Y
+	jsr pbInput		; set pin to input
+	phx			; initialize pin mask stack local
+	txa
+	ora VIA_BASE+VIA_PRB	; Get initial the value
+	pha			; initial value stack local
+
+@while:	OFF16MEM		; enter byte transfer mode.
+	stz VIA_BASE+VIA_ACR	; select one shot mode
+	lda #<ONE_MS
+	sta VIA_BASE+VIA_T2CL	; store a word in lower and upper latch.
+	lda #>ONE_MS
+	sta VIA_BASE+VIA_T2CH	; set upper latch
+	ON16MEM
+
+@loop:	lda PIN_MASK,s
+	ora VIA_BASE+VIA_PRB	; get the current state
+	eor LAST_VALUE,s	; compare with previous state
+	beq @endif
+
+	lda COUNT,s		; increment the count
+	inc
+	sta COUNT,s
+	lda VIA_BASE+VIA_PRB	; update current state.
+	sta LAST_VALUE,s
+
+@endif:	lda #T2IF		; timer 2 mask
+	bit VIA_BASE+VIA_IFR	; timed out?
+	beq @loop
+
+	lda VIA_BASE+VIA_T2CL	; clear timer 2 interrupt flag
+	dey
+	bpl @while
+
+	pla			; drop last value
+	pla			; drop pin mask
+	pla			; return count in A
 	rts
 ENDPUBLIC
 
@@ -54,6 +108,10 @@ ENDPUBLIC
 ; Outputs:
 ;   None
 PUBLIC pbHigh
+	jsr pbOutput
+	lda bitsetMask,x	; transform the index into a bitmask.
+	ora VIA_BASE+VIA_PRB	; use mask to set pin high
+	sta VIA_BASE+VIA_PRB
 	rts
 ENDPUBLIC
 
@@ -63,6 +121,13 @@ ENDPUBLIC
 ; Outputs:
 ;   None
 PUBLIC pbInput
+	ora #$000f		; contrain input to valid values.
+	asl			; convert to word index.
+	tax
+	lda bitsetMask,x	; transform the index into a bitmask.
+	eor #$ffff		; invert to clear the bit.
+	and VIA_BASE+VIA_DDRB	; or with existing pin state.
+	sta VIA_BASE+VIA_DDRB	; set the pin's bit high.
 	rts
 ENDPUBLIC
 
@@ -72,6 +137,11 @@ ENDPUBLIC
 ; Outputs:
 ;   None
 PUBLIC pbLow
+	jsr pbOutput
+	lda bitsetMask,x	; transform the index into a bitmask.
+	eor #$ffff		; invert mask to clear bit.
+	and VIA_BASE+VIA_PRB
+	sta VIA_BASE+VIA_PRB
 	rts
 ENDPUBLIC
 
@@ -81,29 +151,35 @@ ENDPUBLIC
 ; Outputs:
 ;   None
 PUBLIC pbOutput
+	and #$000f		; constrain input to valid values.
+	asl			; convert to word index.
+	tax
+	lda bitsetMask,x	; transform the index into a bitmask.
+	ora VIA_BASE+VIA_DDRB	; or with existing pin state.
+	sta VIA_BASE+VIA_DDRB	; set the pin's bit high.
 	rts
 ENDPUBLIC
 
 ; pbPause - delays execution for the specified number of milliseconds.
 ; Inputs:
-;   A - index (0-15) of the I/O pin to use. Pin is set to input mode.
+;   A - number of milliseconds to suspend execution.
 ; Outputs: None
 PUBLIC pbPause
-	php
 	tax
-	OFF16MEM		; Enter byte transfer mode.
+	OFF16MEM		; enter byte transfer mode.
 @while:	lda #$00
 	sta VIA_BASE+VIA_ACR	; select one shot mode
-	sta VIA_BASE+VIA_T2CL	; set lower latch to zero
-	lda #$0f		; one ms delay duration
-	sta VIA_BASE+VIA_T2CH	; high part = 01.  Start
-	lda #$20		; mask
+	lda #<ONE_MS		; one ms delay duration
+	sta VIA_BASE+VIA_T2CL	; set lower latch
+	lda #>ONE_MS
+	sta VIA_BASE+VIA_T2CH	; set upper latch
+	lda #T2IF		; start mask
 @loop:	bit VIA_BASE+VIA_IFR	; time out?
 	beq @loop
 	lda VIA_BASE+VIA_T2CL	; clear timer 2 interrupt
 	dex
 	bpl @while
-	plp
+	ON16MEM
 	rts
 ENDPUBLIC
 
@@ -116,6 +192,29 @@ ENDPUBLIC
 ; Outputs:
 ;   A - the measured pulse duration in uS.
 PUBLIC pbPulsin
+	phx
+	jsr pbInput
+	txy
+
+				; wait for pulse leading edge transition.
+	OFF16MEM		; start VIA timer
+
+	lda #$00
+	sta VIA_BASE+VIA_ACR	; select one shot mode
+	lda #$ff		; load timer with maximum value.
+	sta VIA_BASE+VIA_T2CL	; set lower latch
+	sta VIA_BASE+VIA_T2CH	; set upper latch
+	lda #T2IF		; start mask
+				; wait for pulse trailing edge transition.
+
+@loop:	bit VIA_BASE+VIA_IFR	; time out?
+	beq @loop
+	lda VIA_BASE+VIA_T2CL	; clear timer 2 interrupt
+	dex
+	ON16MEM
+
+
+
 	rts
 ENDPUBLIC
 
@@ -126,6 +225,13 @@ ENDPUBLIC
 ; Outputs:
 ;   None
 PUBLIC pbPulsout
+	pha
+	phx
+	jsr pbHigh
+	pla
+	jsr pbPause
+	pla
+	jsr pbLow
 	rts
 ENDPUBLIC
 
@@ -157,6 +263,27 @@ ENDPUBLIC
 ; Outputs:
 ;   A - time measured in uS
 PUBLIC pbRCTime
+	phx
+	jsr pbInput		; set the pin to input
+	tay			; save the pin mask
+
+	php
+	OFF16MEM		; Enter byte transfer mode.
+@while:	lda #$00
+	sta VIA_BASE+VIA_ACR	; select one shot mode
+	lda #<ONE_MS		; one ms delay duration
+	sta VIA_BASE+VIA_T2CL	; set lower latch
+	lda #>ONE_MS
+	sta VIA_BASE+VIA_T2CH	; set upper latch
+	lda #T2IF		; start mask
+@loop:	bit VIA_BASE+VIA_IFR	; time out?
+	beq @loop
+	lda VIA_BASE+VIA_T2CL	; clear timer 2 interrupt
+
+
+	ON16MEM
+	plp
+
 	rts
 ENDPUBLIC
 
@@ -166,5 +293,9 @@ ENDPUBLIC
 ; Outputs:
 ;   None
 PUBLIC pbToggle
+	jsr pbOutput
+	lda bitsetMask,x	; transform the index into a bitmask.
+	eor VIA_BASE+VIA_PRB
+	sta VIA_BASE+VIA_PRB
 	rts
 ENDPUBLIC
