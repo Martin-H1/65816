22520586d8f0edff8aaddbe352bb57e209c2ab68
Martin Heermance <mheermance@gmail.com>
2026-02-05 17:20:18 -0500

    Added comments and meaningful labels Added divide by zero handling. bugfix on not returning quotient

--- Mench Reloaded/src/math16.asm
+++ Mench Reloaded/src/math16.asm
@@ -8,45 +8,55 @@ __math16_asm__ = 1
 .include "common.inc"
 .include "math16.inc"
 
-; div16 - 16 bit divison but returns $ffff if you divide by zero
+; div16 - 16 bit integer divison. It works by multipyling divisor by two
+; to get its highest 16 bit multiple. It then conditinally subtracts from
+; the dividend iteratively until the shift count reaches zero.
 ; Inputs:
 ;   C - divisor
 ;   X - dividend
 ; Outputs:
-;   C - quotient
-;   X - remainder
+;   C - quotient ($ffff if you divide by zero)
+;   X - remainder (dividend if you divide by zero)
 ;   Y - clobbered
 PUBLIC div16
 	QUOTIENT = 3
 	DIVISOR = 1
 	pea $0000		; initialize quotient to 0
-	pea $0000		; initialize divisor to 0
+	pea $0000		; reserve space for divisor
 	ldy #1			; initialize shift count to 1
 
-@div1:	asl			; shift divisor: test leftmost bit
-	bcs @div2		; branch when get leftmost 1 bit
-	iny			; otherwise increment shift count
-	cpy #17			; max count (all zeroes in divisor)
-	bne @div1		; loop until all bits processed
+@times2:
+	asl			; 2 * divisor until we get 1 in the carry bit.
+	bcs @maxdivisor
+	iny			; increment the shift count and stop when
+	cpy #17			; all bits are processed. If max count is
+				; reached there are all zeroes in divisor
+	bne @times2
 
-@div2:	ror			; restore shifted-out bit
+	lda #$ffff		; divide by zero error.
+	sta QUOTIENT,s
+	bra @return
 
-				; now divide by subtraction
-@div4:	sta DIVISOR,s		; save divisor
+@maxdivisor:
+	ror			; restore shifted-out bit to divisor
+
+@subtract:			; now divide by subtraction
+	sta DIVISOR,s		; save current divisor
 	txa			; get dividend into the accumulator
 	sec
 	sbc DIVISOR,s		; subtract divisor from dividend
-	bcc @div3		; branch if can't subtract; dividend still in X
+	bcc @skip		; branch if can't subtract; dividend still in X
 	tax 			; store new dividend; carry=1 for quotient
 
-@div3:	lda QUOTIENT,s
-	rol			; shift carry quotient (1 for divide, 0 for not)
+@skip:	lda QUOTIENT,s		; shift carry quotient (1 for divide, 0 for not)
+	rol
 	sta QUOTIENT,s
 	lda DIVISOR,s		; restore divisor
 	lsr			; shift divisor right for next subtract
-	dey			; decrement count
-	bne @div4		; branch to repeat unless count is 0
-	ply			; clean up stack
-	ply			; clean up stack
+	dey			; decrement shift count
+	bne @subtract		; branch to repeat unless count is 0
+@return:
+	pla			; drop divisor
+	pla			; get quotient into C
 	rts
 ENDPUBLIC
