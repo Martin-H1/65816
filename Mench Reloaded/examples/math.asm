ced7e6dfd4e57de20d8f37aad0619cdd2ca2877d
Martin Heermance <mheermance@gmail.com>
2026-02-09 16:41:03 -0500

    Added abs16 and sqrt16 tests.

--- Mench Reloaded/examples/math.asm
+++ Mench Reloaded/examples/math.asm
@@ -18,10 +18,19 @@ PUBLIC main
 	ON16MEM
 	ON16X
 
+	print abs1
+	lda #$fa97		; -1,385 in hex
+	jsr abs16
+	printcdec
+	printcr
+
 	print div1
 	lda #19
 	ldx #367
 	jsr div16
+	printcudec
+	print div3
+	txa
 	printcudec
 	printcr
 
@@ -29,6 +38,9 @@ PUBLIC main
 	lda #17
 	ldx #48317
 	jsr div16
+	printcudec
+	print div3
+	txa
 	printcudec
 	printcr
 
@@ -46,6 +58,24 @@ PUBLIC main
 	printcudec
 	printcr
 
+	print sqrt1
+	lda #537
+	jsr sqrt16
+	printcudec
+	printcr
+
+	print sqrt2
+	lda #1919
+	jsr sqrt16
+	printcudec
+	printcr
+
+	print sqrt3
+	lda #16
+	jsr sqrt16
+	printcudec
+	printcr
+
 	print sub1
 	sec
 	lda #537
@@ -58,9 +88,14 @@ PUBLIC main
 ENDPUBLIC
 
 enter:	.asciiz "Math test enter."
+abs1:	.asciiz "abs(-1385) = "
 div1:	.asciiz "367 / 19 = "
 div2:	.asciiz "48317 / 17 = "
+div3:	.asciiz ", remainder = "
 mul1:	.asciiz "36 * 19 = "
 mul2:	.asciiz "337 * 171 = "
+sqrt1:	.asciiz "sqrt(537) = "
+sqrt2:	.asciiz "sqrt(1919) = "
+sqrt3:	.asciiz "sqrt(16) = "
 sub1:	.asciiz "537 - 1919 = "
 exit:	.asciiz "Math test exit."
