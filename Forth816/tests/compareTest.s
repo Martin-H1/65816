ce7ccbd111eaece6dea5e592e3376a3e0a803d6d
Martin Heermance <mheermance@gmail.com>
2026-03-26 09:56:48 -0400

    Reworked compareTest.s to use interpreter's I/O routines.

--- Forth816/tests/compareTest.s
+++ Forth816/tests/compareTest.s
@@ -12,7 +12,12 @@
 .include "dictionary.inc"
 .include "hal.inc"
 .include "macros.inc"
-.include "print.inc"
+
+.import CR_CODE
+.import DOT_CODE
+.import DOTS_CODE
+.import TYPE_CODE
+.import DOTHEX_CODE
 
 .import EQUAL_CODE
 .import NOTEQUAL_CODE
@@ -30,9 +35,34 @@
 .import LSHIFT_CODE
 .import RSHIFT_CODE
 
+.macro TYPESTR str
+.scope
+	lda #@localstr
+	PUSH
+	lda #.strlen(str)
+	PUSH
+	jsr TYPE_CODE
+	bra @skip
+@localstr: .asciiz str
+@skip:
+.endscope
+.endmacro
+
+.macro TYPESTR_DOT str
+	TYPESTR str
+	jsr DOT_CODE
+	jsr CR_CODE
+.endmacro
+
+.macro TYPESTR_DOTHEX str
+	TYPESTR str
+	jsr DOTHEX_CODE
+	jsr CR_CODE
+.endmacro
+
 ; Main entry point for the test
 PUBLIC MAIN
-	PRINTLN enter
+	TYPESTR "compare test - enter!"
 
 	jsr equalsTest
 	jsr notEqualsTest
@@ -50,12 +80,9 @@ PUBLIC MAIN
 	jsr lshiftTest
 	jsr rshiftTest
 
-	PRINTLN exit
+	TYPESTR "compare test - exit!"
 	rts
 ENDPUBLIC
-
-enter:	.asciiz "compare test - enter!"
-exit:	.asciiz "compare test - exit!"
 
 .proc equalsTest
 	lda #32
@@ -63,18 +90,16 @@ exit:	.asciiz "compare test - exit!"
 	lda #32
 	PUSH
 	jsr EQUAL_CODE
-	PRINTLN_POP  equal1
+	TYPESTR_DOT "= test 32=32 (expect -1) = "
 
 	lda #42
 	PUSH
 	lda #32
 	PUSH
 	jsr EQUAL_CODE
-	PRINTLN_POP equal2
+	TYPESTR_DOT "= test 42=32 (expect 0) = "
 	rts
 .endproc
-equal1:	.asciiz "= test 32=32 (expect FFFF) = "
-equal2:	.asciiz "= test 42=32 (expect 0000) = "
 
 .proc notEqualsTest
 	lda #32
@@ -82,20 +107,16 @@ equal2:	.asciiz "= test 42=32 (expect 0000) = "
 	lda #32
 	PUSH
 	jsr NOTEQUAL_CODE
-	PRINTLN_POP nequal1
+	TYPESTR_DOT "<> test 32=32 (expect 0) = "
 
 	lda #42
 	PUSH
 	lda #32
 	PUSH
 	jsr NOTEQUAL_CODE
-	PRINTLN_POP nequal2
+	TYPESTR_DOT "<> test 42=32 (expect -1) = "
 	rts
 .endproc
-nequal1:
-	.asciiz "<> test 32=32 (expect 0000) = "
-nequal2:
-	.asciiz "<> test 42=32 (expect FFFF) = "
 
 .proc lessThanTest
 	lda #32
@@ -103,39 +124,31 @@ nequal2:
 	lda #32
 	PUSH
 	jsr LESS_CODE
-	PRINTLN_POP less_test1
+	TYPESTR_DOT "< test 32 < 32 (expect 0) = "
 
 	lda #42
 	PUSH
 	lda #32
 	PUSH
 	jsr LESS_CODE
-	PRINTLN_POP less_test2
+	TYPESTR_DOT "< test 42 < 32 (expect 0) = "
 
 	lda #32
 	PUSH
 	lda #42
 	PUSH
 	jsr LESS_CODE
-	PRINTLN_POP less_test3
+	TYPESTR_DOT "< test 32 < 42 (expect -1) = "
 
 	lda #32
 	PUSH
 	lda #$ffbe
 	PUSH
 	jsr LESS_CODE
-	PRINTLN_POP less_test4
+	TYPESTR_DOT "< test 32 < -42 (expect 0) = "
 
 	rts
 .endproc
-less_test1:
-	.asciiz "< test 32 < 32 (expect 0000) = "
-less_test2:
-	.asciiz "< test 42 < 32 (expect 0000) = "
-less_test3:
-	.asciiz "< test 32 < 42 (expect FFFF) = "
-less_test4:
-	.asciiz "< test 32 < -42 (expect 0000) = "
 
 .proc GreaterThanTest
 	lda #32
@@ -143,39 +156,31 @@ less_test4:
 	lda #32
 	PUSH
 	jsr GREATER_CODE
-	PRINTLN_POP gt_test1
+	TYPESTR_DOT "> test 32 > 32 (expect 0) = "
 
 	lda #42
 	PUSH
 	lda #32
 	PUSH
 	jsr GREATER_CODE
-	PRINTLN_POP gt_test2
+	TYPESTR_DOT "> test 42 > 32 (expect -1) = "
 
 	lda #32
 	PUSH
 	lda #42
 	PUSH
 	jsr GREATER_CODE
-	PRINTLN_POP gt_test3
+	TYPESTR_DOT "> test 32 > 42 (expect 0) = "
 
 	lda #32
 	PUSH
 	lda #$ffbe
 	PUSH
 	jsr GREATER_CODE
-	PRINTLN_POP gt_test4
+	TYPESTR_DOT "> test 32 > -42 (expect -1) = "
 
 	rts
 .endproc
-gt_test1:
-	.asciiz "> test 32 > 32 (expect 0000) = "
-gt_test2:
-	.asciiz "> test 42 > 32 (expect FFFF) = "
-gt_test3:
-	.asciiz "> test 32 > 42 (expect 0000) = "
-gt_test4:
-	.asciiz "> test 32 > -42 (expect FFFF) = "
 
 .proc ulessThanTest
 	lda #32
@@ -183,39 +188,31 @@ gt_test4:
 	lda #32
 	PUSH
 	jsr ULESS_CODE
-	PRINTLN_POP uless_test1
+	TYPESTR_DOT "U< test 32 < 32 (expect 0) = "
 
 	lda #42
 	PUSH
 	lda #32
 	PUSH
 	jsr ULESS_CODE
-	PRINTLN_POP uless_test2
+	TYPESTR_DOT "U< test 42 < 32 (expect 0) = "
 
 	lda #32
 	PUSH
 	lda #42
 	PUSH
 	jsr ULESS_CODE
-	PRINTLN_POP uless_test3
+	TYPESTR_DOT "U< test 32 < 42 (expect -1) = "
 
 	lda #32
 	PUSH
 	lda #$ffbe
 	PUSH
 	jsr ULESS_CODE
-	PRINTLN_POP uless_test4
+	TYPESTR_DOT "U< test 32 < -42 (expect -1) = "
 
 	rts
 .endproc
-uless_test1:
-	.asciiz "U< test 32 < 32 (expect 0000) = "
-uless_test2:
-	.asciiz "U< test 42 < 32 (expect 0000) = "
-uless_test3:
-	.asciiz "U< test 32 < 42 (expect FFFF) = "
-uless_test4:
-	.asciiz "U< test 32 < -42 (expect FFFF) = "
 
 .proc uGreaterThanTest
 	lda #32
@@ -223,93 +220,73 @@ uless_test4:
 	lda #32
 	PUSH
 	jsr UGREATER_CODE
-	PRINTLN_POP ugt_test1
+	TYPESTR_DOT "U> test 32 > 32 (expect 0) = "
 
 	lda #42
 	PUSH
 	lda #32
 	PUSH
 	jsr UGREATER_CODE
-	PRINTLN_POP ugt_test2
+	TYPESTR_DOT "U> test 42 > 32 (expect -1) = "
 
 	lda #32
 	PUSH
 	lda #42
 	PUSH
 	jsr UGREATER_CODE
-	PRINTLN_POP gt_test3
+	TYPESTR_DOT "U> test 32 > 42 (expect 0) = "
 
 	lda #32
 	PUSH
 	lda #$ffbe
 	PUSH
 	jsr UGREATER_CODE
-	PRINTLN_POP ugt_test4
+	TYPESTR_DOT "U> test 32 > -42 (expect 0) = "
 
 	rts
 .endproc
-ugt_test1:
-	.asciiz "U> test 32 > 32 (expect 0000) = "
-ugt_test2:
-	.asciiz "U> test 42 > 32 (expect FFFF) = "
-ugt_test3:
-	.asciiz "U> test 32 > 42 (expect 0000) = "
-ugt_test4:
-	.asciiz "U> test 32 > -42 (expect 0000) = "
 
 .proc zeroEqualsTest
 	lda #32
 	PUSH
 	jsr ZEROEQ_CODE
-	PRINTLN_POP zequal1
+	TYPESTR_DOT "0= 32 test (expect 0) = "
 
 	lda #0000
 	PUSH
 	jsr ZEROEQ_CODE
-	PRINTLN_POP zequal2
+	TYPESTR_DOT "0= 0 equals (expect -1) = "
 
 	rts
 .endproc
-zequal1:
-	.asciiz "0= 32 test (expect 0000) = "
-zequal2:
-	.asciiz "0= 0 equals (expect FFFF) = "
 
 .proc zeroLessTest
 	lda #32
 	PUSH
 	jsr ZEROLESS_CODE
-	PRINTLN_POP zeroless1
+	TYPESTR_DOT "0< 32 test (expect 0) = "
 
 	lda #$ffbe
 	PUSH
 	jsr ZEROLESS_CODE
-	PRINTLN_POP zeroless2
+	TYPESTR_DOT "0< -42 test (expect -1) = "
 
 	rts
 .endproc
-zeroless1:
-	.asciiz "0< 32 test (expect 0000) = "
-zeroless2:
-	.asciiz "0< -42 test (expect FFFF) = "
 
 .proc zeroGtTest
 	lda #32
 	PUSH
 	jsr ZEROGT_CODE
-	PRINTLN_POP zerogt1
+	TYPESTR_DOT "0> 32 test (expect -1) = "
 
 	lda #$ffbe
 	PUSH
 	jsr ZEROGT_CODE
-	PRINTLN_POP zerogt2
+	TYPESTR_DOT "0> -42 test (expect 0) = "
 
 	rts
 .endproc
-zerogt1:
-	.asciiz "0> 32 test (expect FFFF) = "
-zerogt2:
-	.asciiz "0> -42 test (expect 0000) = "
 
 .proc andTest
 	lda #$ff00
@@ -317,11 +294,10 @@ zerogt2:
 	lda #$0ff0
 	PUSH
 	jsr AND_CODE
-	PRINTLN_POP and1
+	TYPESTR_DOTHEX "AND test ff00 and 0ff0 (expect 0F00) = "
 
 	rts
 .endproc
-and1:	.asciiz "AND test ff00 and 0ff0 (expect 0F00) = "
 
 .proc orTest
 	lda #$ff00
@@ -329,11 +305,10 @@ and1:	.asciiz "AND test ff00 and 0ff0 (expect 0F00) = "
 	lda #$0ff0
 	PUSH
 	jsr OR_CODE
-	PRINTLN_POP or1
+	TYPESTR_DOTHEX "OR test ff00 and 0ff0 (expect FFF0) = "
 
 	rts
 .endproc
-or1:	.asciiz "OR test ff00 and 0ff0 (expect FFF0) = "
 
 .proc xorTest
 	lda #$ff00
@@ -341,22 +316,19 @@ or1:	.asciiz "OR test ff00 and 0ff0 (expect FFF0) = "
 	lda #$0ff0
 	PUSH
 	jsr XOR_CODE
-	PRINTLN_POP xor1
+	TYPESTR_DOTHEX "XOR test ff00 and 0ff0 (expect F0F0) = "
 
 	rts
 .endproc
-xor1:	.asciiz "XOR test ff00 and 0ff0 (expect F0F0) = "
 
 .proc invertTest
 	lda #$f0f0
 	PUSH
 	jsr INVERT_CODE
-	PRINTLN_POP invert1
+	TYPESTR_DOTHEX "Invert test f0f0 (expect 0F0F) = "
 
 	rts
 .endproc
-invert1:
-	.asciiz "Invert test f0f0 (expect 0F0F) = "
 
 .proc lshiftTest
 	lda #32
@@ -364,21 +336,17 @@ invert1:
 	lda #0
 	PUSH
 	jsr LSHIFT_CODE
-	PRINTLN_POP lshift1
+	TYPESTR_DOTHEX "lshift test 0020 0 (expect 0020) = "
 
 	lda #32
 	PUSH
 	lda #3
 	PUSH
 	jsr LSHIFT_CODE
-	PRINTLN_POP lshift2
+	TYPESTR_DOTHEX "lshift test 0020 3  (expect 0100) = "
 
 	rts
 .endproc
-lshift1:
-	.asciiz "lshift test 0020 0 (expect 0020) = "
-lshift2:
-	.asciiz "lshift test 0020 3  (expect 0100) = "
 
 .proc rshiftTest
 	lda #32
@@ -386,18 +354,14 @@ lshift2:
 	lda #0
 	PUSH
 	jsr RSHIFT_CODE
-	PRINTLN_POP rshift1
+	TYPESTR_DOTHEX "rshift test 0020 0 (expect 0020) = "
 
 	lda #32
 	PUSH
 	lda #3
 	PUSH
 	jsr RSHIFT_CODE
-	PRINTLN_POP rshift2
+	TYPESTR_DOTHEX "rshift test 0020 3 (expect 0004) = "
 
 	rts
 .endproc
-rshift1:
-	.asciiz "rshift test 0020 0 (expect 0020) = "
-rshift2:
-	.asciiz "rshift test 0020 3 (expect 0004) = "
