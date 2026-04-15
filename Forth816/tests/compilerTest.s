973490e2972a7584e47830d8ddda876e76eb9d67
Martin Heermance <mheermance@gmail.com>
2026-04-15 12:47:02 -0400

    Added unit tests for :, ;, CREATE, DOES>, CONSTANT, and VARIABLE

--- Forth816/tests/compilerTest.s
+++ Forth816/tests/compilerTest.s
@@ -15,9 +15,7 @@
 .include "macrosdbg.inc"
 
 .import COLON_CODE
-.import SEMICOLON_CODE
-.import RBRACKET_CODE
-.import LBRACKET_CODE
+.import MOVE_CODE
 
 .importzp UP
 .importzp W
@@ -25,25 +23,42 @@
 ; Main entry point for the test
 PUBLIC MAIN
 	TYPESTRCR "Compiler test - enter!"
+
 	jsr colonTest
-	jsr semicolonTest
-	jsr rbracketTest
-	jsr lbracketTest
+	jsr constantTest
+	jsr createDoesTest
+	jsr variableTest
 
 	TYPESTRCR "Compiler test - exit!"
 	rts
 ENDPUBLIC
 
 .proc colonTest
-.endproc
-
-.proc semicolonTest
-.endproc
-
-.proc rbracketTest
+	MOVE_TIB "  : foo 42 ; foo                "
+	CALL_DOCOL INTERPRET_CFA	; RTS_CFA will return here.
+	TYPESTR_DOT "Compile test of ': foo 32 ;  foo .' (expect 42) = "
 	rts
 .endproc
 
-.proc lbracketTest
+.proc constantTest
+	MOVE_TIB "55 constant limit limit         "
+	CALL_DOCOL INTERPRET_CFA	; RTS_CFA will return here.
+	TYPESTR_DOT "Compile test of '55 constant limit limit' (expect 55) = "
+	rts
+.endproc
+
+.proc createDoesTest
+	MOVE_TIB ": kons create , does> @ ;   "
+	CALL_DOCOL INTERPRET_CFA	; RTS_CFA will return here.
+	MOVE_TIB "55 kons limit   limit       "
+	CALL_DOCOL INTERPRET_CFA	; RTS_CFA will return here.
+	TYPESTR_DOT "Compile test of 'create does' (expect 55) = "
+	rts
+.endproc
+
+.proc variableTest
+	MOVE_TIB "VARIABLE DATE 12 DATE ! DATE @  "
+	CALL_DOCOL INTERPRET_CFA	; RTS_CFA will return here.
+	TYPESTR_DOT "Compile test of 'VARIABLE DATE 12 DATE ! DATE @' (expect 12) = "
 	rts
 .endproc
