ee7ed176a58730e001a45701d835ad7fd15d7b2f
Martin Heermance <mheermance@gmail.com>
2026-05-02 14:14:54 -0400

    Added additional test cases to allow deprecation of assembly test.

--- Forth816/tests/tests/stack_test.fs
+++ Forth816/tests/tests/stack_test.fs
@@ -34,4 +34,24 @@ T{ 1 2 3 4 2SWAP -> 3 4 1 2 }T
 \ 2OVER
 T{ 1 2 3 4 2OVER -> 1 2 3 4 1 2 }T
 
+\ PICK
+T{ 1 2 3 4 1 2 PICK -> 1 2 3 4 1 3 }T
+
+\ DEPTH
+T{ 1 2 3 4 1 3 PICK -> 1 2 3 4 1 2 }T
+
+CLEAR-STACK
+
+: TEST>R  ( n -- n ) >R R> ;
+
+T{ 42 TEST>R -> 42 }T
+
+: TEST>R2  ( a b -- b a ) >R >R R> R> ;
+
+T{ 1 2 TEST>R2 -> 1 2 }T
+
+: TESTR@  ( n -- n n ) >R R@ R> ;
+
+T{ 42 TESTR@ -> 42 42 }T
+
 DONE
